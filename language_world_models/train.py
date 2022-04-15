7938e8051ff2383dda3bc94dca2ced01c862e176
SHU Yoshifumi <envzhu@gmail.com>
2022-04-15 21:54:44 +0900

    訓練コードのリファクタリング

--- language_world_models/train.py
+++ language_world_models/train.py
@@ -17,12 +17,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 @torch.no_grad()
-def test(args, env, model_vae, i_episode):
-    # 1エピソードの実行
-    obs_listener_ep, obs_speaker_ep, reward_ep, done_ep = util.play_one_episode(env)
-    
-    # VAEのテスト
-    x = torch.stack(obs_listener_ep).to(device)
+def test_vae(args, model_vae, i_episode, x):
+    model_vae.eval()
     _, _, _, recon_x = model_vae(x)
 
     # 入力画像の表示
@@ -44,16 +40,28 @@ def test(args, env, model_vae, i_episode):
 
     plt.close(fig)
 
+
+@torch.no_grad()
+def test(args, env, model_vae, i_episode):
+    # 1エピソードの実行
+    obs_listener_ep, obs_speaker_ep, reward_ep, done_ep = util.play_one_episode(env)
+    
+    # VAEのテスト
+    x = torch.stack(obs_listener_ep).to(device)
+    test_vae(args, model_vae, i_episode, x)
+
+
 # VAE の更新
-def train_vae_step(model_vae, optimizer_vae, x):
+def train_batch_vae(model_vae, optimizer_vae, x):
     model_vae.train()
     model_vae.zero_grad()
 
     _, z_mean, z_logstd, recon_x = model_vae(x)
-    loss_vae, KL_loss, recon_loss = model_vae.loss(x, recon_x, z_mean, z_logstd)
-    loss_vae.backward()
+    vae_loss, kl_loss, recon_loss = model_vae.loss(x, recon_x, z_mean, z_logstd)
+    vae_loss.backward()
     optimizer_vae.step()
-    return loss_vae, KL_loss, recon_loss
+    
+    return vae_loss.cpu().detach().numpy(), kl_loss.cpu().detach().numpy(), recon_loss.cpu().detach().numpy()
 
 
 def train(args):
@@ -61,8 +69,8 @@ def train(args):
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
 
-    obs_listener_list, obs_speaker_list, reward_list, done_list = [], [], [], [],
-    losses_vae = []
+    obs_listener_list, obs_speaker_list = [], []
+    losses = {'vae' : {'loss':[], 'kl_loss':[], 'recon_loss':[]}}
 
     # ゲーム環境のインスタンスの取得
     env = create_ChoosePathGridDefaultEnv()
@@ -84,27 +92,30 @@ def train(args):
 
         obs_listener_list.extend(obs_listener_ep)
         obs_speaker_list.extend(obs_speaker_ep)
-        reward_list.extend(reward_ep)
-        done_list.extend(done_ep)
 
         # バッチサイズ分のデータが集まったらパラメータの更新
         if len(obs_listener_list) >= args.batch_size:
             # VAE の更新
-            x = torch.stack(obs_listener_list).to(device)
-            loss_vae, KL_loss, recon_loss = train_vae_step(model_vae, optimizer_vae, x)
-
-            losses_vae.append(loss_vae.cpu().detach().numpy())
-        
+            x = torch.stack(obs_listener_list[:args.batch_size]).to(device)
+            loss, kl_loss, recon_loss = train_batch_vae(model_vae, optimizer_vae, x)
+            losses['vae']['loss'].append(loss)
+            losses['vae']['kl_loss'].append(kl_loss)
+            losses['vae']['recon_loss'].append(recon_loss)
+       
             del obs_listener_list[:args.batch_size]
             del obs_speaker_list[:args.batch_size]
-            del reward_list[:args.batch_size] # TODO
-            del done_list[:args.batch_size]   # TODO
         
         if (i_episode+1) % args.print_freq == 0:
-            print('episode: %d / %d (%d sec) \tVAE : Train Lower Bound: %lf  (KL loss : %lf,  Reconstruction loss : %lf)' %
-                  (i_episode+1, args.num_episodes, time.time()-last_time, np.average(losses_vae),  KL_loss, recon_loss))
+            print('episode: %d / %d (%d sec)' % (i_episode+1, args.num_episodes, time.time()-last_time))
             last_time = time.time()
-            losses_vae = []
+            
+            # VAE について表示
+            print(losses['vae']['loss'])
+            print('\tVAE : Train Lower Bound: %lf  (KL loss : %lf,  Reconstruction loss : %lf)' %
+                  (np.average(losses['vae']['loss']),  np.average(losses['vae']['kl_loss']), np.average(losses['vae']['recon_loss'])))
+            losses['vae']['loss'].clear()
+            losses['vae']['kl_loss'].clear()
+            losses['vae']['recon_loss'].clear()
 
         if (i_episode+1) % args.save_freq == 0:
             model_vae_path = os.path.join(args.checkpoints_dir, "model_vae_%06d.pth" % (i_episode+1))
